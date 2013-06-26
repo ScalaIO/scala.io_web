fbf3c06e2613fe8bd8298c5a536ee5b160874bf9
Olivier Girardot <ssaboum@gmail.com>
2013-06-26 21:07:44 +0200

    add: theme + base logos

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+	// Fancybox setup
+    $("a.js-fancybox").fancybox({
+       openEffect: 'elastic',
+       closeEffect: 'elastic'
+    });
+    //bootstrap tooltip setup
+    $('[rel="tooltip"]').tooltip();
+});
